03348c156a23904478483da5e5f6b13baa65574a
Thiago Castro <thiagopac@MacBook-Pro-de-Thiago.local>
2014-02-16 20:06:11 -0300

    Commit inicial. Projeto em branco. Pods adicionados.

---
+++ Pubsee/Pubsee/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pubsee
+//
+//  Created by Thiago Castro on 16/02/14.
+//  Copyright (c) 2014 Thiago Castro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pubsee/Pubsee/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pubsee
+//
+//  Created by Thiago Castro on 16/02/14.
+//  Copyright (c) 2014 Thiago Castro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
